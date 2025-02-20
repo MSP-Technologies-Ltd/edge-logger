e5c46c04f752ce3dc28aeb62254696deae52d72b
Catriona Ferguson <catriona@multisourcepower.com>
2025-02-20 11:14:17 +0000

    test

--- new_main.py
+++ new_main.py
@@ -7,7 +7,6 @@ from dotenv import load_dotenv
 from pathlib import Path
 import json
 import aioredis
-from collections import ChainMap
 
 load_dotenv()
 
@@ -24,7 +23,7 @@ REDIS_ROUTING_KEYS = "#.commands.#"
 
 csv_queue = asyncio.Queue()
 redis_queue = asyncio.Queue()
-latest_messages = {} 
+latest_messages = {}
 
 
 async def init_redis():
@@ -49,7 +48,9 @@ async def declare_and_bind_redis(channel: aio_pika.Channel):
     queue_name = "command_logging"
     queue = await channel.declare_queue(queue_name, durable=True)
     await queue.bind(EXCHANGE_NAME, routing_key=REDIS_ROUTING_KEYS)
-    print(f"Command Logging Queue bound to exchange {EXCHANGE_NAME} with routing key {REDIS_ROUTING_KEYS}")
+    print(
+        f"Command Logging Queue bound to exchange {EXCHANGE_NAME} with routing key {REDIS_ROUTING_KEYS}"
+    )
     return queue
 
 
@@ -60,12 +61,13 @@ async def consume_and_store_redis(channel: aio_pika.Channel):
         async for message in queue_iter:
             async with message.process():
                 try:
-                    body = message.body.decode()
-                    data = json.loads(body)
+                    pass
+                    # body = message.body.decode()
+                    # data = json.loads(body)
 
-                    device_id = data.get("deviceId") or data.get("client_id")
-                    if device_id:
-                        await save_to_redis(device_id, data)
+                    # device_id = data.get("deviceId") or data.get("client_id")
+                    # if device_id:
+                    #     await save_to_redis(device_id, data)
                 except Exception as e:
                     print(f"Error processing Redis message: {e}")
 
@@ -73,17 +75,19 @@ async def consume_and_store_redis(channel: aio_pika.Channel):
 async def declare_and_bind_logging(channel: aio_pika.Channel):
     queue_name = "logging"
     queue = await channel.declare_queue(
-        queue_name, 
-        durable=True, 
+        queue_name,
+        durable=True,
         arguments={
-            'x-max-length': 5, 
-            'x-message-ttl': 5000, 
-            'x-consumer-timeout-action': 'ack', 
-            'x-overflow': 'drop-head'
-        }
+            "x-max-length": 5,
+            "x-message-ttl": 5000,
+            "x-consumer-timeout-action": "ack",
+            "x-overflow": "drop-head",
+        },
     )
     await queue.bind(EXCHANGE_NAME, routing_key=LOGGING_ROUTING_KEYS)
-    print(f"Logging Queue bound to exchange {EXCHANGE_NAME} with routing key {LOGGING_ROUTING_KEYS}")
+    print(
+        f"Logging Queue bound to exchange {EXCHANGE_NAME} with routing key {LOGGING_ROUTING_KEYS}"
+    )
     return queue
 
 
@@ -94,20 +98,54 @@ async def consume_logging(channel: aio_pika.Channel):
         async for message in queue_iter:
             async with message.process():
                 try:
+                    headers = dict(message.headers)
+
                     body = message.body.decode()
                     data = dict(json.loads(body))
 
-                    device_id = data.get("deviceId") or data.get("clientId")
+                    timestamp = None
+                    device_id = None
+
+                    if headers:
+                        # print(headers)
+
+                        if "timestamp" or "Timestamp" in headers:
+                            timestamp = headers.get("timestamp") or headers.get(
+                                "timestamp"
+                            )
+
+                        if "deviceId" in headers:
+                            device_id = headers.get("deviceId")
+
+                    if not headers:
+                        if "deviceId" or "clientId" in data:
+                            device_id = data.get("deviceId") or data.get("clientId")
+                    # device_id = data.get("deviceId") or data.get("clientId")
 
                     # message = data['message']
-                    if 'message' in data:
-                        new_data = data['message']
+                    if "message" in data:
+                        new_data = None
+                        message_data = data["message"]
 
-                        
-                        if not device_id:
-                            continue
+                        # deal with accuvim
+                        if "event" in data:
+                            new_data = data["body"]["message"]
+                            new_data.pop("rawValues", None)
+                            timestamp = data["body"]["timestamp"]
+                            device_id = data["headers"]["deviceId"]
 
-                    latest_messages[device_id] = new_data  # Store only the latest message
+                        if timestamp is not None:
+                            timestamp_data = {"timestamp": timestamp}
+                            new_data = timestamp_data.update(message_data)
+
+                        else:
+                            new_data = message_data
+                        # new_data = data["message"]
+
+                        if device_id is not None:
+                            latest_messages[device_id] = (
+                                new_data  # Store only the latest message
+                            )
 
                 except Exception as e:
                     print(f"Error processing logging message: {e}")
@@ -120,7 +158,9 @@ async def save_csv_file(keys, data, device_id):
     documents_path = Path.home() / "Documents" / "Logs" / device_id
     documents_path.mkdir(parents=True, exist_ok=True)
 
-    target_file = documents_path / f"log_{device_id}_{current_hour.strftime('%Y-%m-%d_%H')}.csv"
+    target_file = (
+        documents_path / f"log_{device_id}_{current_hour.strftime('%Y-%m-%d_%H')}.csv"
+    )
 
     mode = "a" if target_file.exists() else "w"
     with target_file.open(mode=mode, newline="") as doc:
@@ -137,37 +177,38 @@ async def save_messages():
         if latest_messages:
             for device_id, data in latest_messages.items():
                 # timestamp = data.get("timestamp")
-                print(f"Saving log for {device_id}")
 
                 keys = list(data.keys())
 
-                if 'logged_action' not in keys:
-                    if 'rawValues' in keys:
-                        raw_values = data['rawValues']
-                        print(raw_values)
-                        raw_keys = list(raw_values.keys())
-                        
-                        new_data = data.pop('rawValues', None)
+                # print(device_id, keys)
 
+                if "event" in keys:
+                    pass
+                if "logged_action" not in keys:
+                    if "rawValues" in keys:
+                        raw_values = data["rawValues"]
+                        # print(raw_values)
+                        raw_keys = list(raw_values.keys())
+
+                        new_data = data.pop("rawValues", None)
+
+                        print(f"Saving log for {device_id}")
                         await save_csv_file(keys, new_data, device_id)
-                        await save_csv_file(raw_keys, raw_values, 'meterRawValues')
+                        await save_csv_file(raw_keys, raw_values, "meterRawValues")
                 else:
+                    print(f"Saving log for {device_id}")
                     await save_csv_file(keys, data, device_id)
 
                 # print(keys)
-                    # keys.insert(0, "timestamp")
+                # keys.insert(0, "timestamp")
 
                 # for key in keys:
                 #     rel_data[key] = data[key]
-                    
-                    
-                    # d = {'timestamp': timestamp, **rel_data}
-                    
-                    # print(keys)
 
-                 
-                
- 
+                # d = {'timestamp': timestamp, **rel_data}
+
+                # print(keys)
+
         await asyncio.sleep(2)
 
 
@@ -204,7 +245,9 @@ async def app():
                 # Start consumers
                 consumer_task = asyncio.create_task(consume_logging(channel))
                 writer_task = asyncio.create_task(save_messages())
-                redis_consumer_task = asyncio.create_task(consume_and_store_redis(channel))
+                redis_consumer_task = asyncio.create_task(
+                    consume_and_store_redis(channel)
+                )
 
                 await asyncio.gather(consumer_task, writer_task, redis_consumer_task)
         except Exception as e:
