8277635ad62947a05d9ca36f68a53a424c660076
Catriona Ferguson <catriona@multisourcepower.com>
2025-03-10 16:51:19 +0000

    Moving to additional file - extras

--- logger.py
+++ logger.py
@@ -7,6 +7,7 @@ from dotenv import load_dotenv
 from pathlib import Path
 import json
 import aioredis
+from extras import process_device_data, get_device_id
 
 load_dotenv()
 
@@ -76,20 +77,23 @@ async def consume_and_store_redis(channel: aio_pika.Channel):
         async for message in queue_iter:
             async with message.process():
                 try:
+                    headers = message.headers
                     body = message.body.decode()
                     data = json.loads(body)
 
-                    device_id = data.get("deviceId")
+                    combined_message = {**headers, **data} if headers else data
 
-                    if device_id is None:
-                        device_id = data.get("client_id")
+                    device_id = get_device_id(combined_message)
 
                     if device_id is not None:
-                        await save_to_redis(device_id, data)
+                        if "heartbeat_echo" in combined_message:
+                            await save_to_redis(f"{device_id}-heartbeat", data)
+                        elif "heartbeat_echo" not in combined_message:
+                            keys = combined_message.keys()
 
-                        keys = data.keys()
-
-                        await save_csv_file(keys, data, device_id, commands=True)
+                            await save_csv_file(
+                                keys, combined_message, device_id, commands=True
+                            )
 
                 except Exception as e:
                     print(
@@ -129,69 +133,41 @@ async def consume_logging(channel: aio_pika.Channel):
             async with message.process():
                 try:
                     # decode message body
+                    headers = message.headers
                     body = message.body.decode()
 
                     data = json.loads(body)
 
-                    # get device id from body
-                    device_id = data.get("deviceId")
+                    combined_message = {**headers, **data}
 
-                    # if device id not found, check headers
-                    if device_id is None:
-                        headers = message.headers
+                    device_id = get_device_id(combined_message)
 
-                        timestamp = headers.get("timestamp")
-                        device_id = headers.get("device_id")
+                    if device_id is not None:
+                        processed_data = await process_device_data(
+                            device_id, combined_message
+                        )
 
-                        # if timestamp doesn't exist in data, add at time of processing
-                        if timestamp is None:
-                            timestamp = datetime.now().isoformat()
+                        if processed_data is not None:
+                            if type(processed_data) is list:
+                                for data in processed_data:
+                                    data_type = data.get("type")
 
-                        # primarily for dealing with batteries
-                        if device_id is None:
-                            device_id = headers.get("Unique ID")
+                                    if data_type is not None:
+                                        if data_type == "standard_data":
+                                            latest_messages[
+                                                f"{device_id} - unparsed data"
+                                            ] = data
 
-                        message_data = data.get("message")
-
-                        # create device info object to add to existing data
-                        device_info = {
-                            "deviceId": device_id,
-                            "timestamp": str(timestamp),
-                        }
-
-                        # battery data
-                        if message_data is not None:
-                            if "standard" in message_data:
-
-                                standard_data = message_data.get("standard")
-                                unparsed_data = message_data.get("data")
-                                parsed_data = message_data.get("parsed data")
-
-                                unparsed_data = {**device_info, **unparsed_data}
-                                standard_data = {**device_info, **standard_data}
-                                parsed_data = {**device_info, **parsed_data}
-
-                                if unparsed_data is not None:
-                                    latest_messages[f"{device_id} - unparsed data"] = (
-                                        unparsed_data
-                                    )
-                                if parsed_data is not None:
-                                    latest_messages[f"{device_id} - parsed data"] = (
-                                        parsed_data
-                                    )
-                                if standard_data is not None:
-                                    latest_messages[f"{device_id} - standard data"] = (
-                                        standard_data
-                                    )
-                        # inverter data
-                        else:
-                            device_id = data.get("client_id")
-
-                            latest_messages[device_id] = data
-                    # controller data
-                    else:
-                        if "globalState" in data:
-                            latest_messages[device_id] = data
+                                        elif data_type == "unparsed_data":
+                                            latest_messages[
+                                                f"{device_id} - unparsed data"
+                                            ] = data
+                                        elif data_type == "parsed_data":
+                                            latest_messages[
+                                                f"{device_id} - parsed data"
+                                            ] = data
+                            else:
+                                latest_messages[device_id] = processed_data
 
                 except Exception as e:
                     print(f"Error processing logging message: {e}, message: {data}")
@@ -205,10 +181,10 @@ async def save_csv_file(keys, data, device_id, commands):
     current_hour = now.replace(minute=0, second=0, microsecond=0)
 
     # for local testing (not in docker)
-    # log_dir = "./logs"
+    log_dir = "./logs"
 
     # logs set up for docker container.
-    log_dir = "/app/Logs"
+    # log_dir = "/app/Logs"
 
     documents_path = (
         Path(log_dir) / f"{device_id}"
@@ -216,13 +192,8 @@ async def save_csv_file(keys, data, device_id, commands):
         else Path(log_dir) / "commands" / f"{device_id}"
     )
 
-    # if not commands:
-    #     documents_path = Path(log_dir) / f"{device_id}"
-    # else:
-    #     documents_path = Path(log_dir) / "commands" / f"{device_id}"
-
     documents_path.mkdir(parents=True, exist_ok=True)
-    
+
     # target file name dependent on whether it is a command log or not
     target_file = (
         (documents_path / f"log_{device_id}_{current_hour.strftime('%Y-%m-%d_%H')}.csv")
@@ -233,23 +204,13 @@ async def save_csv_file(keys, data, device_id, commands):
         )
     )
 
-    # if not commands:
-    #     target_file = (
-    #         documents_path
-    #         / f"log_{device_id}_{current_hour.strftime('%Y-%m-%d_%H')}.csv"
-    #     )
-    # else:
-    #     target_file = (
-    #         documents_path
-    #         / f"commands_{device_id}_{current_hour.strftime('%Y-%m-%d_%H')}.csv"
-    #     )
-
     mode = "a" if target_file.exists() else "w"
     with target_file.open(mode=mode, newline="") as doc:
         writer = csv.DictWriter(doc, fieldnames=keys)
         if mode == "w":
             writer.writeheader()
         writer.writerow(data)
+
 
 # iterates through latest messages, provides keys and data to save_csv_file
 # occurs every 2 seconds (status logging only)
@@ -264,6 +225,7 @@ async def save_messages():
                 # print(f"Saving log for {device_id}")
 
         await asyncio.sleep(2)
+
 
 # method to connect to RabbitMQ with retries
 async def connect_with_retry(max_retries=5, base_delay=2):
@@ -282,6 +244,7 @@ async def connect_with_retry(max_retries=5, base_delay=2):
             wait_time = base_delay * (2 ** (retries - 1))
             print(f"Connection failed ({e}). Retrying in {wait_time:.2f} seconds")
             await asyncio.sleep(wait_time)
+
 
 # main method to run the app
 async def app():
